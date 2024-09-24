615ac8b9dbdd2b77579201218d0fd6199ea8d7d4
tracer-ics2024 <tracer@njuics.org>
2024-09-24 14:21:51 +0800

    >  compile NEMU 2022211985 初叙辰 Linux ics-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:21:51 up 4 days,  6:46,  5 users,  load average: 0.11, 0.06, 0.07

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -24,6 +24,9 @@ static int is_batch_mode = false;
 
 void init_regex();
 void init_wp_pool();
+void wp_display();
+void wp_set(char*, word_t);
+void wp_delete(int);
 
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 static char* rl_gets() {
@@ -56,6 +59,62 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args);
+
+static int cmd_info(char *args);
+
+static int cmd_x(char *args);
+
+static int cmd_p(char *args);
+
+static int cmd_w(char *args);
+
+static int cmd_d(char *args);
+
+static struct {
+  const char *name;
+  const char *description;
+  int (*handler) (char *);
+} cmd_table [] = {
+  { "help", "Display information about all supported commands", cmd_help },
+  { "c", "Continue the execution of the program", cmd_c },
+  { "q", "Exit NEMU", cmd_q },
+  { "si", "si [N], Execute N(default one) step", cmd_si },
+  { "info", "info SUBCMD, Print current state of (r)register or (w)watchpoint", cmd_info },
+  { "x", "x N EXPR, Print data from memory address EXPR to EXPR+4N per 4 Bytes", cmd_x },
+  { "p", "p EXPR, Caculate the value of expression EXPR", cmd_p },
+  { "w", "w EXPR, Stop executing when EXPR changed", cmd_w },
+  { "d", "d N, Delete Nrd watchpoints", cmd_d },
+
+  /* TODO: Add more commands */
+
+};
+
+#define NR_CMD ARRLEN(cmd_table)
+
+static int cmd_help(char *args) {
+  /* extract the first argument */
+  char *arg = strtok(NULL, " ");
+  int i;
+
+  if (arg == NULL) {
+    /* no argument given */
+    for (i = 0; i < NR_CMD; i ++) {
+      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+    }
+  }
+  else {
+    for (i = 0; i < NR_CMD; i ++) {
+      if (strcmp(arg, cmd_table[i].name) == 0) {
+        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+        return 0;
+      }
+    }
+    printf("Unknown command '%s'\n", arg);
+  }
+  return 0;
+}
+
 static int cmd_si(char *args) {
     /* extract the first argument */
     char *arg = strtok(NULL, " ");
@@ -81,7 +140,7 @@ static int cmd_info(char *args) {
     if (*arg == 'r')
 	isa_reg_display();
     else if (*arg == 'w')
-	printf("This command should print the current state of watchpoint but TODO now\n");
+	wp_display();
     else
 	printf("Unknown options and please input \"help info\"\n");
     return 0;
@@ -125,46 +184,26 @@ static int cmd_p(char *args) {
     return 0;
 }
 
-static struct {
-  const char *name;
-  const char *description;
-  int (*handler) (char *);
-} cmd_table [] = {
-  { "help", "Display information about all supported commands", cmd_help },
-  { "c", "Continue the execution of the program", cmd_c },
-  { "q", "Exit NEMU", cmd_q },
-  { "si", "si [N], Execute N(default one) step", cmd_si },
-  { "info", "info SUBCMD, Print current state of (r)register or (w)watchpoint", cmd_info },
-  { "x", "x N EXPR, Print data from memory address EXPR to EXPR+4N per 4 Bytes", cmd_x },
-  { "p", "p EXPR, Caculate the value of expression EXPR", cmd_p}
-
-  /* TODO: Add more commands */
-
-};
-
-#define NR_CMD ARRLEN(cmd_table)
-
-static int cmd_help(char *args) {
-  /* extract the first argument */
-  char *arg = strtok(NULL, " ");
-  int i;
-
-  if (arg == NULL) {
-    /* no argument given */
-    for (i = 0; i < NR_CMD; i ++) {
-      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+static int cmd_w(char *args) {
+    bool success;
+    word_t value = expr(args, &success);
+    if (!success) {
+	printf("Invalid expression\n");
+	assert(0);
     }
-  }
-  else {
-    for (i = 0; i < NR_CMD; i ++) {
-      if (strcmp(arg, cmd_table[i].name) == 0) {
-        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-        return 0;
-      }
+    wp_set(args, value);
+    return 0;
+}
+
+static int cmd_d(char *args) {
+    char *endptr;
+    int n = strtol(args, &endptr, 10);
+    if (*endptr != '\0') {
+	printf("Please input in the format like \"d N\", N is a positive integer\n");
+	return 0;
     }
-    printf("Unknown command '%s'\n", arg);
-  }
-  return 0;
+    wp_delete(n);
+    return 0;
 }
 
 void sdb_set_batch_mode() {
--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -19,6 +19,10 @@
 
 typedef struct watchpoint {
   int NO;
+  char *EXPR;
+  word_t last_value;
+  word_t current_value;
+  char *is_changed;
   struct watchpoint *next;
 
   /* TODO: Add more members if necessary */
@@ -28,10 +32,43 @@ typedef struct watchpoint {
 static WP wp_pool[NR_WP] = {};
 static WP *head = NULL, *free_ = NULL;
 
+WP* new_wp() {
+    assert(free_ != NULL);
+    WP *new_wp = free_;
+    free_ = free_->next;
+    new_wp->next = head;
+    head = new_wp;
+    return new_wp;
+}
+
+void free_wp(WP *wp) {
+    assert(wp != NULL);
+    WP *h = head;
+    if (h == wp) {
+	if (h->next == NULL)
+	    head = NULL;
+	else
+	    h = h->next;
+    } else {
+	while (h && h->next != wp)
+	    h = h->next;
+	if (h == NULL) {
+	    printf("No this watchpoint to free\n");
+	    return;
+	}
+	h->next = wp->next;
+    }
+    wp->next = free_;
+    free_ = wp;
+}
+
 void init_wp_pool() {
   int i;
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
+    wp_pool[i].last_value = 0;
+    wp_pool[i].current_value = 0;
+    wp_pool[i].is_changed = "False";
     wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
   }
 
@@ -41,3 +78,30 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+void wp_display() {
+    WP *h = head;
+    if (h == NULL) {
+	printf("No watchpoints");
+	return;
+    }
+    printf("NO\tEXPR\tlast_value\tcurrent_value\tis_changed\n");
+    while (h) {
+	printf("%d\t%s\t%u\t%u\t%s\n", h->NO, h->EXPR, h->last_value, h->current_value, h->is_changed);
+	h = h -> next;
+    }
+}
+
+void wp_set(char *args, word_t value) {
+    WP *wp = new_wp();
+    wp->EXPR = args;
+    wp->last_value = value;
+    wp->current_value = value;
+    wp->is_changed = "False";
+    printf("Set %drd watchpoint in %s, its value is %u\n", wp->NO, wp->EXPR, wp->current_value);
+}
+
+void wp_delete(int n) {
+    WP *p = &wp_pool[n];
+    free_wp(p);
+    printf("Delete %drd watchpoint", n);    
+}
